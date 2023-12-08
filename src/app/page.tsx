02e0fa85c19e229fcf37de171a953c91f9438453
Sebastian Słoma <sebastiansloma@gmail.com>
2023-12-08 11:27:53 +0100

    styled header top

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
 	return (
 		<div>
 			<Button
-      disableRipple
+   
       className="relative overflow-visible rounded-full hover:-translate-y-1 px-12 shadow-xl bg-background/30 after:content-[''] after:absolute after:rounded-full after:inset-0 after:bg-background/40 after:z-[-1] after:transition after:!duration-500 hover:after:scale-150 hover:after:opacity-0"
       size="lg"
       >Click me</Button>
--- src/components/HeaderTop.tsx
+++ src/components/HeaderTop.tsx
@@ -1,54 +1,51 @@
-import React from 'react'
-import { BsFacebook, BsTwitter, BsInstagram, BsLinkedin } from "react-icons/bs"
+import React from 'react';
+import { BsFacebook, BsTwitter, BsInstagram, BsLinkedin } from 'react-icons/bs';
 
 const HeaderTop = () => {
-  return (
-    <div className="border-b border-gray-200 hidden sm:block">
-      <div className="container py-4">
-        <div className="flex justify-between items-center">
-          <div className="hidden lg:flex gap-1">
-            
-              <BsFacebook />
-            
-            
-              <BsTwitter />
-           
-           
-              <BsInstagram />
-            
-          
-              <BsLinkedin />
-           
-          </div>
+	return (
+		<div className='border-b border-gray-200 hidden sm:block'>
+			<div className='container py-4'>
+				<div className='flex justify-between items-center'>
+					<div className='hidden lg:flex gap-1'>
+						<div className='header_icon'>
+							<BsFacebook />
+						</div>
+						<div className='header_icon'>
+							<BsTwitter />
+						</div>
+						<div className='header_icon'>
+							<BsInstagram />
+						</div>
+						<div className='header_icon'>
+							<BsLinkedin />
+						</div>
+					</div>
 
-          <div className="text-gray-500 text-[12px]">
-            <b>FREE SHIPPING</b> THIS WEEK ORDER OVER - €100
-          </div>
+					<div className='text-gray-500 text-[12px]'>
+						<b>FREE SHIPPING</b> THIS WEEK ORDER OVER - €100
+					</div>
 
-          <div className="flex gap-4">
-            <select
-              className="text-gray-500 text-[12px] w-[70px]"
-              name="currency"
-              id="currency"
-            >
-              <option value="EUR €">EUR €</option>
-              <option value="USD $">USD $</option>
-              
-            </select>
+					<div className='flex gap-4'>
+						<select
+							className='text-gray-500 text-[12px] w-[70px]'
+							name='currency'
+							id='currency'>
+							<option value='EUR €'>EUR €</option>
+							<option value='USD $'>USD $</option>
+						</select>
 
-            <select
-              className="text-gray-500 text-[12px] w-[80px]"
-              name="language"
-              id="language"
-            >
-              <option value="English">English</option>
-              <option value="French">Español</option>
-            </select>
-          </div>
-        </div>
-      </div>
-    </div>
-  )
-}
+						<select
+							className='text-gray-500 text-[12px] w-[80px]'
+							name='language'
+							id='language'>
+							<option value='English'>English</option>
+							<option value='Español'>Español</option>
+						</select>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
 
-export default HeaderTop
+export default HeaderTop;
