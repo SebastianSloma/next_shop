01fd3207e4be95f9684370d081bf297c3e7a6746
Sebastian Słoma <sebastiansloma@gmail.com>
2023-12-09 12:39:15 +0100

    added keen-slider

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,9 +1,63 @@
-import React from 'react'
+'use client';
 
-const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
+import * as React from 'react';
+import { Image } from '@nextui-org/react';
+import { useKeenSlider } from 'keen-slider/react';
+import 'keen-slider/keen-slider.min.css';
+
+export default function App() {
+	const [sliderRef] = useKeenSlider<HTMLDivElement>(
+		{
+			loop: true,
+		},
+		[
+			slider => {
+				let timeout: ReturnType<typeof setTimeout>;
+				let mouseOver = false;
+				function clearNextTimeout() {
+					clearTimeout(timeout);
+				}
+				function nextTimeout() {
+					clearTimeout(timeout);
+					if (mouseOver) return;
+					timeout = setTimeout(() => {
+						slider.next();
+					}, 2000);
+				}
+				slider.on('created', () => {
+					slider.container.addEventListener('mouseover', () => {
+						mouseOver = true;
+						clearNextTimeout();
+					});
+					slider.container.addEventListener('mouseout', () => {
+						mouseOver = false;
+						nextTimeout();
+					});
+					nextTimeout();
+				});
+				slider.on('dragStarted', clearNextTimeout);
+				slider.on('animationEnded', nextTimeout);
+				slider.on('updated', nextTimeout);
+			},
+		]
+	);
+
+	return (
+		<>
+			<div ref={sliderRef} className='keen-slider'>
+				<div className='keen-slider__slide'>
+					<Image width={300} alt='NextUI hero Image' src='/a18.png' />
+				</div>
+				<div className='keen-slider__slide'>
+					<Image width={300} alt='NextUI hero Image' src='/a17.png' />
+				</div>
+				<div className='keen-slider__slide'>
+					<Image width={300} alt='NextUI hero Image' src='/a16.png' />
+				</div>
+				<div className='keen-slider__slide'>
+					<Image width={300} alt='NextUI hero Image' src='/a15.png' />
+				</div>
+			</div>
+		</>
+	);
 }
-
-export default Hero
