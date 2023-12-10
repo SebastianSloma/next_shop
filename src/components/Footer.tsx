099c8129352b3928bb084f5d3c36bab80fe8ef73
Sebastian Słoma <sebastiansloma@gmail.com>
2023-12-10 19:39:13 +0100

    added link to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,14 @@
+import { Link } from '@nextui-org/react'
 import React from 'react'
 
 const Footer = () => {
   return (
-    <div className=" text-gray-500 text-center py-4 pb-16 md:pb-4">
-    Copyright © https://github.com/SebastianSloma <br /> All Rights Reserved 2023.
+    <div className=" text-gray-500 text-center py-6 pb-16 md:pb-4">
+    Copyright © 
+
+    <Link color='warning' isExternal href="https://github.com/SebastianSloma" >github.com/SebastianSloma</Link>
+
+     <br /> All Rights Reserved 2023.
   </div>
   )
 }
