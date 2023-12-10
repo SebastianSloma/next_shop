b7f262894d54740bcdfc394ef7b6aec67869dfbc
Sebastian Słoma <sebastiansloma@gmail.com>
2023-12-10 13:03:54 +0100

    added footer tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Footer from '@/components/Footer';
 import Hero from '@/components/Hero';
 import NewOffer from '@/components/NewOffer';
 import Products from '@/components/Products';
@@ -12,6 +13,7 @@ export default function Home() {
 			<Products/>
 			<Testimonials/>
 			<NewOffer/>
+			<Footer/>
 		</main>
 
 
