809636d66174553b986f669b27d6bf6afaf62a94
Sebastian Słoma <sebastiansloma@gmail.com>
2023-12-10 13:07:13 +0100

    .

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className="bg-blackish text-gray-500 text-center py-4 pb-16 md:pb-4">
+    <div className=" text-gray-500 text-center py-4 pb-16 md:pb-4">
     Copyright © https://github.com/SebastianSloma <br /> All Rights Reserved 2023.
   </div>
   )
